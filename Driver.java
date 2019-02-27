79f7014d38da44c45bab967cb401c4dd8c7a7c11
Jeremy Lee <jlee00@stuy.edu>
2019-02-27 13:40:54 -0500

    Added Maze.java

--- Driver.java
+++ Driver.java
@@ -3,20 +3,20 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Driver{
   public static void main(String args[]){
-    int numLines = 0;
+    int numRows = 0;
     int numCols = 0;
     try {
       File maze1 = new File("Maze1.txt");
       Scanner inf = new Scanner(maze1);
       while (inf.hasNextLine()){
         String line = inf.nextLine();
-        numLines++;
+        numRows++;
         numCols = line.length();
         System.out.println(line);
       }
     } catch (FileNotFoundException e){
       System.out.println("File not found");
     }
-    System.out.println(numLines + " x " + numCols);
+    System.out.println(numRows + " x " + numCols);
   }
 }
